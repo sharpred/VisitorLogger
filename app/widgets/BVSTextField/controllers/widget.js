14b012e60a712acaf7445573bc458ce3d5f2e021
Paul Ryan <paul.ryan@stepupsoftware.co.uk>
2016-06-14 15:38:45 +0100

    keyboard

--- app/widgets/BVSTextField/controllers/widget.js
+++ app/widgets/BVSTextField/controllers/widget.js
@@ -4,29 +4,13 @@ if (args.hintText) {
     $.txt.hintText = args.hintText;
 }
 
-var send = Titanium.UI.createButton({
-    title : 'Next',
-    style : Titanium.UI.iPhone.SystemButtonStyle.DONE,
-});
-
-var back = Titanium.UI.createButton({
-    title : 'Back',
-    style : Titanium.UI.iPhone.SystemButtonStyle.DONE,
-});
-
-var cancel = Titanium.UI.createButton({
-    systemButton : Titanium.UI.iPhone.SystemButton.CANCEL
-});
-
-var flexSpace = Titanium.UI.createButton({
-    systemButton : Titanium.UI.iPhone.SystemButton.FLEXIBLE_SPACE
-});
-var buttons = [cancel, flexSpace, back, flexSpace, send];
-
 if (args.isFirst) {
-    buttons = [cancel, flexSpace, send];
+    $.back.disabled = false;
 }
-$.txt.keyboardToolbar = buttons;
+
+if (args.nextAction && _.isFunction(args.nextAction)) {
+    $.done.addEventListener("click", args.nextAction);
+}
 
 $.setValue = function(val) {
     $.txt.value = val;
